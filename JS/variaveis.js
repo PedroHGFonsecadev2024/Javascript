f9d84e1b5a12bcc60af1de3bd59055fa3c7a77df
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-03-13 11:05:07 -0300

    Teste de variáveis JS

---
+++ JS/variaveis.js
@@ -0,0 +1,8 @@
+function exemploVar(){
+    if(true){
+        var x=10; let y = 20; const z =30;
+    }
+    console.log(x) //acessível
+    console.log(y) //Erro: y não está definido
+    console.log(z) //Erro: z não está definido
+}
