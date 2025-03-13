dde54450e5ee4710ab72ef941a76684f266c8e6d
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-03-13 15:52:18 -0300

    protótipo do exercício 9

---
+++ JS/ex09.js
@@ -0,0 +1,8 @@
+'use strict'
+function mediaInserir(){
+    var array = [] ;
+    var userinput = document.getElementById("userinput");
+    array.push(userinput);
+    array.forEach((element) => {
+        console.log(element)
+});}
