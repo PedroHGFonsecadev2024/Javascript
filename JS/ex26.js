4221e1cb6e5ed7589517910c0ded8c27e7fb3c8e
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-03-27 15:56:16 -0300

    Exercício 26

--- JS/ex26.js
+++ JS/ex26.js
@@ -1,3 +1,10 @@
 const arraypar = (input) => {
-  
+    let array1 = input.split(",")
+    let array2 = []
+    for (let i = 0; i <= array1.length; i++){
+        if (array1[i] % 2 == 0){
+            array2.push(array1[i])
+        }
+    }
+    resultado.textContent = "Pares:" + array2 
 }
