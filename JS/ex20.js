4b5656a50827ea4f7a9713008b9fe2ba1523f475
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-03-27 13:44:57 -0300

    exercício 20 e exemplo de arrow function

--- JS/ex20.js
+++ JS/ex20.js
@@ -1,15 +1,13 @@
-function numPrimVer(){
-    const numero = parseFloat(document.getElementById("numero").value) 
-    let resultado = document.getElementById("resultado")
-    for (let i = numero; i >= 1; i--){
-        if (numero % i == 0){
-            var contador = contador++
+function numPrimVer() {
+    let num = parseInt(document.getElementById("numero").value);
+    let primo = num > 1;
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            primo = false;
+            break;
         }
     }
-    console.log(contador)
-    if (contador > 2){
-        resultado.textContent = "Não é primo"
-    } else if (contador = 2) {
-        resultado.textContent = "É primo"
-    }
+    //operador ternário
+    let resultado = primo ? "É primo" : "Não é primo";
+    document.getElementById("resultado").textContent = resultado;
 }
